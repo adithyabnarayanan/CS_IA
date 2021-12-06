10ebb61660583cee321c796eb5d3257ecb37191e
Ronit <ronitbuti.ofs@gmail.com>
2021-12-06 13:25:32 +0800

    Update StudentMeetings.java

--- src/main/java/cs_ia/StudentMeetings.java
+++ src/main/java/cs_ia/StudentMeetings.java
@@ -3,10 +3,10 @@ package cs_ia;
 import java.util.ArrayList;
 
 public class StudentMeetings {
-    private String name;
-    private int noOfMeetings = 0;
-    private ArrayList<String> meetingNotes = new ArrayList<>();
-    private ArrayList<TimeSlot> TimeSlots = new ArrayList<>();
+    private String name; // name of the student
+    private int noOfMeetings = 0; // number of meetings
+    private ArrayList<String> meetingNotes = new ArrayList<>(); // list of meeting notes
+    private ArrayList<TimeSlot> TimeSlots = new ArrayList<>(); // list of time slots
 
     public void saveMeetingToFile() {
 
